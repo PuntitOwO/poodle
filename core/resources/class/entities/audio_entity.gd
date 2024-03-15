7a933a17e493f0e3524b43fca584c95d0a39d55f
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-03-15 12:51:16 -0300

    Add Audio/ImageEntity

---
+++ core/resources/class/entities/audio_entity.gd
@@ -0,0 +1,31 @@
+# 1. class name: fill the class name
+class_name AudioEntity
+extends Entity
+
+# 2. docs: use docstring (##) to generate docs for this file
+## An [Entity] that holds the reference to an audio file
+
+# 3. signals: define signals here
+
+# 4. enums: define enums here
+
+# 5. constants: define constants here
+
+# 6. export variables: define all export variables in groups here
+@export_file() var audio_path: String
+# 7. public variables: define all public variables here
+var id: int
+# 8. private variables: define all private variables here, use _ as preffix
+
+# 9. onready variables: define all onready variables here
+
+
+# 10. init virtual methods: define _init, _enter_tree and _ready mothods here
+
+# 11. virtual methods: define other virtual methos here
+
+# 12. public methods: define all public methods here
+
+# 13. private methods: define all private methods here, use _ as preffix
+
+# 14. subclasses: define all subclasses here
