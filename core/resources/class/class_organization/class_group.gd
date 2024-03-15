8f30a8e89adf7460cf0185b587f19b6cc55623d3
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-03-15 13:05:30 -0300

    Add class resources

---
+++ core/resources/class/class_organization/class_group.gd
@@ -0,0 +1,31 @@
+# 1. class name: fill the class name
+class_name ClassGroup
+extends Resource
+
+# 2. docs: use docstring (##) to generate docs for this file
+## A resource that represents a group of [Entity]s
+
+# 3. signals: define signals here
+
+# 4. enums: define enums here
+
+# 5. constants: define constants here
+
+# 6. export variables: define all export variables in groups here
+@export var entities: Array[Entity]
+# 7. public variables: define all public variables here
+
+# 8. private variables: define all private variables here, use _ as preffix
+
+# 9. onready variables: define all onready variables here
+
+
+# 10. init virtual methods: define _init, _enter_tree and _ready mothods here
+
+# 11. virtual methods: define other virtual methos here
+
+# 12. public methods: define all public methods here
+
+# 13. private methods: define all private methods here, use _ as preffix
+
+# 14. subclasses: define all subclasses here
