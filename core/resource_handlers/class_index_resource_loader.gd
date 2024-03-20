edc91bc6016c3ea3f9fe75fd9b7f518ca4f221cc
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-03-19 22:18:03 -0300

    Add ClassIndexResourceLoader/Saver classes

---
+++ core/resource_handlers/class_index_resource_loader.gd
@@ -0,0 +1,49 @@
+@tool
+class_name ClassIndexResourceLoader
+extends ResourceFormatLoader
+
+## ClassIndexResourceLoader is a custom resource loader for [ClassIndex] resources.
+## It loads a JSON file and deserializes it into a [ClassIndex] resource.
+
+## Returns the list of extensions available for loading the resource.
+## In this case, only json.
+func _get_recognized_extensions() -> PackedStringArray:
+    return PackedStringArray(["json"])
+
+## Returns the resource type that this loader can handle.
+## In this case, only custom resources that are a subclass of [Resource].
+func _get_resource_type(_path: String) -> String:
+    return "Resource"
+
+## Returns the class name of the script that will be created when loading the resource.
+## In this case, [ClassIndex].
+func _get_resource_script_class(_path: String) -> String:
+    return "ClassIndex"
+
+## Returns whether the resource type is handled by this loader.
+## In this case, only custom resources that are a subclass of [Resource].
+func _handles_type(type: StringName) -> bool:
+    return ClassDB.is_parent_class(type, "Resource")
+
+## Loads the resource from the given file.
+## In this case, it reads the JSON file and deserializes it into a [ClassIndex] resource.
+## Returns an [ClassIndex] resource on success, or an error code on failure.
+## Only path parameter is currently used, other parameters are not used.
+func _load(path: String, _original_path: String, _use_sub_threads: bool, _cache_mode: int):
+    var file: FileAccess = FileAccess.open(path, FileAccess.READ)
+    if file == null:
+        return FileAccess.get_open_error()
+    
+    var raw_json: String = file.get_as_text()
+    file.close()
+
+    var json: JSON = JSON.new()
+    var error: int = json.parse(raw_json)
+    if error != OK:
+        return error
+    if typeof(json.data) != TYPE_DICTIONARY:
+        return ERR_INVALID_DATA
+    
+    var class_index: ClassIndex = ClassIndex.new()
+    class_index.deserialize(json.data)
+    return class_index
---
+++ core/resource_handlers/class_index_resource_saver.gd
@@ -0,0 +1,35 @@
+@tool
+class_name ClassIndexResourceSaver
+extends ResourceFormatSaver
+
+## ClassIndexResourceSaver is a custom resource saver for [ClassIndex] resources.
+## It saves the resource to a JSON file.
+
+## Returns the list of extensions available for saving the resource,
+## in this case, only json.
+func _get_recognized_extensions(_resource: Resource) -> PackedStringArray:
+    return PackedStringArray(["json"])
+
+## Returns whether the resource can be saved by this saver.
+## In this case, only [ClassIndex] resources can be saved.
+func _recognize(resource: Resource) -> bool:
+    return resource is ClassIndex
+
+## Saves the resource to a file at the given path.
+## In this case, the resource is serialized to JSON and saved to the file.
+## Returns @GlobalScope.OK on success, or an error code on failure.
+## The flags are currently unused.
+func _save(resource: Resource, path: String, _flags: int):
+    if not (resource is ClassIndex):
+        return ERR_INVALID_DATA
+    
+    var data: Dictionary = resource.serialize()
+    var json: String = JSON.stringify(data)
+
+    var file: FileAccess = FileAccess.open(path, FileAccess.WRITE)
+    if file == null:
+        return FileAccess.get_open_error()
+    
+    file.store_string(json)
+    file.close()
+    return OK
