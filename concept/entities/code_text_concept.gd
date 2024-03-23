8cf7648ec1a51d635aa25b118b776c5a27ba27c4
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-03-22 23:46:49 -0300

    code text style

--- concept/entities/code_text_concept.gd
+++ concept/entities/code_text_concept.gd
@@ -7,6 +7,10 @@ func _ready():
 	_play()
 
 func _play():
-	var code_block: CodeEdit = CodeEdit.new()
+	var code_block: TextEdit = TextEdit.new()
+	var highlighter: CodeHighlighter = preload("res://concept/code_highlighter/godot_editor_gdscript.tres")
 	code_block.text = resource.content
+	code_block.editable = false
+	code_block.syntax_highlighter = highlighter
+	code_block.custom_minimum_size = Vector2(300, 300)
 	add_child(code_block)
