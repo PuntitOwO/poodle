f5ff8caace731b2b52527d14ac4129c75fb02fa3
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-03-28 18:27:43 -0300

    cache duration computation

--- core/widgets/group_controllers/chain_group_controller.gd
+++ core/widgets/group_controllers/chain_group_controller.gd
@@ -3,17 +3,23 @@ extends GroupController
 
 ## A [GroupController] that runs its children in sequence.
 
+var _duration: float = 0.0
+var _durations: Array[float] = []
+
 ## Computes the duration of this group.
 ## It is the sum of the duration of all the children.
 func compute_duration() -> float:
-    var duration = 0.0
-    for child in get_children():
-        duration += child.compute_duration()
-    return duration
+    if is_zero_approx(_duration):
+        for child in get_children():
+            var duration = child.compute_duration()
+            _duration += duration
+            _durations.append(duration)
+    return _duration
 
 ## Runs the children in sequence.
 ## It waits for each children to finish before starting the next.
-func play(duration: float) -> void:
-    for child in get_children():
-        child.play(duration)
+func play(__duration: float) -> void:
+    for child_idx in get_child_count():
+        var child = get_child(child_idx)
+        child.play(_durations[child_idx])
         await child.animation_finished
--- core/widgets/group_controllers/parallel_group_controller.gd
+++ core/widgets/group_controllers/parallel_group_controller.gd
@@ -3,14 +3,16 @@ extends GroupController
 
 ## A [GroupController] that runs its children in parallel.
 
+var _duration: float = 0.0
+
 ## Computes the duration of this group.
 ## The duration of a parallel group is the maximum duration of all its children.
 func compute_duration() -> float:
-    var max_duration = 0.0
-    for child in get_children():
-        if child.has_method("compute_duration"):
-            max_duration = max(max_duration, child.compute_duration())
-    return max_duration
+    if is_zero_approx(_duration):
+        for child in get_children():
+            if child.has_method("compute_duration"):
+                _duration = max(_duration, child.compute_duration())
+    return _duration
 
 ## Runs all children in parallel.
 ## This means that all children will be played at the same time.
