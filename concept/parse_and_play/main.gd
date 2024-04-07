b0be2cac95ee1df4264e46183bd673100b38056d
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-04-06 23:41:28 -0300

    Windows maximize on class load

--- concept/parse_and_play/main.gd
+++ concept/parse_and_play/main.gd
@@ -4,10 +4,10 @@ extends Node
 @onready var window: ConceptClassWindow = $ClassWindow
 
 func _ready():
+    DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
     _setup_scene()
 
 func _setup_scene():
-    print("Setting up scene")
     window.set_class_node(class_scene)
     window.set_section_tree(class_scene.section_manager.tree)
     class_scene.compute_duration()
