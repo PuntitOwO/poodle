3a9d8f3422a3633f397dd4ed0e19ee463255e719
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-04-25 23:57:59 -0400

    Line record advancement

--- concept/line_record/line_record.gd
+++ concept/line_record/line_record.gd
@@ -30,8 +30,9 @@ func _handle_mouse(event: InputEventMouseMotion):
         line.add_point(event.global_position)
         pressed = false
         last_point = Vector2.INF
-        _post_process_line(line)
+        var thumbnail := _post_process_line(line)
         entities.append(_generate_entity(line))
+        %ItemList.add_item("Line " + str(entities.size()), thumbnail)
         line = null
 
 func _new_line() -> Line2D:
@@ -41,14 +42,28 @@ func _new_line() -> Line2D:
     _line.joint_mode = Line2D.LINE_JOINT_ROUND
     return _line
 
-func _post_process_line(node: Line2D) -> void:
-    # Get the top left coordinate of the box of the line
+func _post_process_line(node: Line2D) -> Texture2D:
+    # Get the bounding box of the line
     var l: float = INF;
     var t: float = INF;
+    var r: float = -INF;
+    var b: float = -INF;
     for i in node.get_point_count():
         var p := node.get_point_position(i)
         l = min(l, p.x)
         t = min(t, p.y)
+        r = max(r, p.x)
+        b = max(b, p.y)
+    # Take a screenshot of the bounding box
+    var screenshot := get_viewport().get_texture().get_image()
+    # Get screenshot coordinates of the bounding box
+    var viewport_size := Vector2i(get_viewport().get_visible_rect().size)
+    var screenshot_size := screenshot.get_size()
+    var ratio := viewport_size / screenshot_size
+    var screenshot_rect := Rect2i(Vector2i(Vector2(l, t)) * ratio, Vector2i(Vector2(r, b)) * ratio)
+    # Crop the screenshot
+    screenshot = screenshot.get_region(screenshot_rect)
+
     # Move the line to the origin
     var line_position := Vector2(l, t)
     for i in node.get_point_count():
@@ -56,6 +71,9 @@ func _post_process_line(node: Line2D) -> void:
         node.set_point_position(i, p - line_position)
     # Move the node to compensate visually for the line movement
     node.position += line_position
+    # Create a new texture with the cropped screenshot
+    return ImageTexture.create_from_image(screenshot)
+
 
 func _generate_entity(node: Line2D) -> LineEntity:
     var entity = LineEntity.new()
