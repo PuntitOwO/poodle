12cd0435e15f11b2d382f9df371c0529b981cc31
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-05-11 23:23:54 -0400

    Entity properties editor v2

--- editor/entities/entity_wrapper_editor_window.gd
+++ editor/entities/entity_wrapper_editor_window.gd
@@ -6,8 +6,46 @@ extends Window
         _update_preview()
         _update_properties()
 
+func _ready():
+    (%RefreshButton as Button).pressed.connect(_update_preview)
+
 func _update_properties():
-    pass
+    var prop_list := %PropertyEditor as Control
+    for child in prop_list.get_children():
+        prop_list.remove_child(child)
+        child.queue_free()
+    if not is_instance_valid(entity_wrapper):
+        return
+    var prop_types := CustomClassDB.get_inheriters_from_class(&"EntityProperty")
+    for prop in entity_wrapper.entity_properties:
+        var index := prop_types.find(prop.get_class_name())
+        prop_types.remove_at(index)
+        # TODO: add property to the property editor
+        var container := HBoxContainer.new()
+        var label := Label.new()
+        label.text = prop.get_class_name()
+        container.add_child(label)
+        var button := Button.new()
+        button.text = "Remove"
+        button.pressed.connect(_remove_property.bind(prop))
+        container.add_child(button)
+        %PropertyEditor.add_child(container)
+    for prop_type in prop_types:
+        var button := Button.new()
+        button.text = "Add " + prop_type
+        button.pressed.connect(_add_property.bind(prop_type))
+        %PropertyEditor.add_child(button)
+
+func _add_property(prop_type: String):
+    var prop := CustomClassDB.instantiate(prop_type) as EntityProperty
+    entity_wrapper.entity_properties.append(prop)
+    _update_properties()
+    _update_preview()
+
+func _remove_property(prop: EntityProperty):
+    entity_wrapper.entity_properties.erase(prop)
+    _update_properties()
+    _update_preview()
 
 func _update_preview():
     var preview := %PreviewWhiteboard as Node2D
