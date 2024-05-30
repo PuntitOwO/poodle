0c018540f1a9a2d4f6883161ad9ac750a2f78490
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-05-30 17:34:15 -0400

    Update example player mod

--- mods/example_mod/player/example_player_tab.gd
+++ mods/example_mod/player/example_player_tab.gd
@@ -1,6 +1,9 @@
 extends Control
 
 @onready var break_button: Button = %BreakButton
+@onready var pause_overlay: CanvasLayer = %PauseOverlay
+@onready var resume_button: Button = %ResumeButton
+@onready var progress_clock: TextureProgressBar = %ProgressClock
 @onready var counter: Label = %Counter
 
 const BREAK_DURATION = 5 * 60
@@ -9,6 +12,8 @@ var break_controller: Tween
 
 func _ready():
     break_button.pressed.connect(_break)
+    resume_button.pressed.connect(_stop_break)
+    progress_clock.max_value = BREAK_DURATION
 
 func _break():
     if break_controller:
@@ -20,13 +25,16 @@ func _break():
 
 func _start_break():
     get_tree().paused = true
-    counter.show()
+    pause_overlay.visible = true
 
 func _stop_break():
     get_tree().paused = false
-    counter.hide()
+    pause_overlay.visible = false
+    if break_controller:
+        break_controller.kill()
 
 func _update_counter(value: int):
+    progress_clock.value = value
     var mins = value / 60
     var secs = value % 60
     counter.text = str(mins).pad_zeros(2) + ":" + str(secs).pad_zeros(2)
