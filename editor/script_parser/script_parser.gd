53dcd9a16e4a4e56bd35f70671eddb09bd4629ee
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-05-30 23:07:34 -0400

    Add compute sections and entities from script

--- editor/script_parser/script_parser.gd
+++ editor/script_parser/script_parser.gd
@@ -10,6 +10,8 @@ var slide_counter := 1
 
 func _ready():
     %SaveButton.pressed.connect(parse)
+    %SectionsButton.pressed.connect(compute_sections_ask)
+    %EntitiesButton.pressed.connect(compute_entities)
     editor_signals = Engine.get_singleton(&"EditorSignals") as EditorEventBus
     editor_signals.class_index_changed.connect(_on_class_index_changed)
     editor_signals.class_script_changed.connect(_on_class_script_changed)
@@ -48,6 +50,46 @@ func remove_until(line: String, until: String) -> String:
 func parse():
     script_entries = parse_text()
     editor_signals.class_script_change_requested.emit(script_entries)
+
+func compute_sections_ask():
+    var dialog: ConfirmationDialog = ConfirmationDialog.new()
+    add_child(dialog)
+    dialog.title = "Confirm action: Compute Sections"
+    dialog.dialog_text = "This action will compute the sections from the current script.\nIt will replace your current section data.\nAre you sure you want to proceed?"
+    dialog.confirmed.connect(compute_sections)
+    dialog.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
+    dialog.popup()
+
+func compute_sections():
+    var sections: Array[ClassSection] = []
+    for entry in parse_text():
+        if not entry is CommentEntry: continue
+        var line := entry.content
+        if line.begins_with("Slide"):
+            line = remove_until(line, ": ")
+            if sections.is_empty():
+                printerr("No section for slide")
+                continue
+            var section := sections[-1]
+            var new_slide := ClassSlide.new()
+            new_slide.name = line
+            section.slides.append(new_slide)
+        elif line.begins_with("Sec"):
+            line = remove_until(line, ": ")
+            var new_section := ClassSection.new()
+            new_section.name = line
+            sections.append(new_section)
+    editor_signals.class_sections_change_requested.emit(sections)
+
+func compute_entities():
+    var entities: Array[Entity] = []
+    for entry in parse_text():
+        if not entry is TextEntry: continue
+        var line := entry.content
+        var new_entity := TextEntity.new()
+        new_entity.content = line
+        entities.append(new_entity)
+    editor_signals.class_entities_add_requested.emit(entities)
 
 func parse_text() -> Array[ScriptEntry]:
     var entries: Array[ScriptEntry] = []
