a809e1c9f0062bbf6f5bc78a0cdb93852580a621
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-06-06 21:15:16 -0400

    Add settings screen and mod security alert. Mods no longer get automatically loaded.

--- core/mods/module_loader.gd
+++ core/mods/module_loader.gd
@@ -4,94 +4,113 @@ var mod_folder_exists = false
 
 var mods_loaded: Array[ModProperties] = []
 
+var mod_alert_accepted: bool
+
 ## Returns the mods folder, creating it if it doesn't exist.
 ## If the folder couldn't be created, mod_folder_exists will be false.
 func get_mods_folder() -> DirAccess:
-    var dir := DirAccess.open("user://")
-    if not dir.dir_exists("mods"):
-        dir.make_dir_recursive("mods")
-    var status := dir.change_dir("mods")
-    if status == OK:
-        mod_folder_exists = true
-    return dir
+	var dir := DirAccess.open("user://")
+	if not dir.dir_exists("mods"):
+		dir.make_dir_recursive("mods")
+	var status := dir.change_dir("mods")
+	if status == OK:
+		mod_folder_exists = true
+	return dir
 
 ## Returns a list of all mods in the mods folder.
 func get_mods_list() -> Array[ModPath]:
-    var dir := get_mods_folder()
-    if not mod_folder_exists:
-        return []
-    var mods: Array[ModPath] = []
-    for folder in dir.get_directories():
-        var mod_path := ModPath.from_dir(dir, folder)
-        if mod_path.is_valid:
-            mods.append(mod_path)
-    return mods
+	var dir := get_mods_folder()
+	if not mod_folder_exists:
+		return []
+	var mods: Array[ModPath] = []
+	for folder in dir.get_directories():
+		var mod_path := ModPath.from_dir(dir, folder)
+		if mod_path.is_valid:
+			mods.append(mod_path)
+	return mods
 
 ## Loads a mod from the mods folder. Returns true on success.
 func load_mod(mod_path: ModPath) -> bool:
-    var full_path := "user://mods/" + mod_path.folder + "/" + mod_path.mod_file
-    return ProjectSettings.load_resource_pack(full_path, false)
+	var full_path := "user://mods/" + mod_path.folder + "/" + mod_path.mod_file
+	return ProjectSettings.load_resource_pack(full_path, false)
 
 ## Loads all mods from the mods folder. Returns the quantity of mods loaded.
 func load_all_mods() -> int:
-    var mods := get_mods_list()
-    var success = 0
-    for mod in mods:
-        if load_mod(mod) and add_loaded_mod(mod):
-            success += 1
-    CustomClassDB.force_update_classes()
-    return success
+	var mods := get_mods_list()
+	var success = 0
+	for mod in mods:
+		if load_mod(mod) and add_loaded_mod(mod):
+			success += 1
+	CustomClassDB.force_update_classes()
+	return success
 
 func add_loaded_mod(mod_path: ModPath) -> bool:
-    var full_path := "res://mods/" + mod_path.folder + "/manifest.tres"
-    var manifest := load(full_path) as ModProperties
-    if manifest == null: return false
-    mods_loaded.append(manifest)
-    return true
+	var full_path := "res://mods/" + mod_path.folder + "/manifest.tres"
+	var manifest := load(full_path) as ModProperties
+	if manifest == null: return false
+	mods_loaded.append(manifest)
+	return true
 
 func _ready():
-    load_all_mods()
-    if OS.has_feature("editor"):
-        load_local_mods()
+	check_security_alert_status()
+
+func load_mods():
+	load_all_mods()
+	if OS.has_feature("editor"):
+		load_local_mods()
+
+#region Security Alert
+
+func check_security_alert_status():
+	var dir := DirAccess.open("user://")
+	mod_alert_accepted = dir.file_exists(".modaccepted")
+
+func accept_security_alert():
+	mod_alert_accepted = true
+	var file := FileAccess.open("user://.modaccepted", FileAccess.WRITE)
+	file.store_string("User accepted on " + Time.get_datetime_string_from_system())
+	file.close()
+
+#endregion
 
 #region Local Mods
 
 func load_local_mods():
-    print("Loading local mods")
-    var mods := get_local_mods_folder().get_directories()
-    for mod in mods:
-        var mod_path := ModPath.new()
-        mod_path.folder = mod
-        if add_loaded_mod(mod_path):
-            print("Loaded local mod: " + mod)
+	print("Loading local mods")
+	var mods := get_local_mods_folder().get_directories()
+	for mod in mods:
+		var mod_path := ModPath.new()
+		mod_path.folder = mod
+		if add_loaded_mod(mod_path):
+			print("Loaded local mod: " + mod)
 
 ## Returns the local mods folder, creating it if it doesn't exist.
 func get_local_mods_folder() -> DirAccess:
-    var dir := DirAccess.open("res://")
-    if not dir.dir_exists("mods"):
-        dir.make_dir_recursive("mods")
-    dir.change_dir("mods")
-    return dir
+	var dir := DirAccess.open("res://")
+	if not dir.dir_exists("mods"):
+		dir.make_dir_recursive("mods")
+	dir.change_dir("mods")
+	return dir
 
 #endregion
 
 ## Class representing a mod in the mods folder.
 class ModPath:
-    var folder: String
-    var mod_file: String
+	var folder: String
+	var mod_file: String
 
-    var has_mod_file: bool:
-        get:
-            return mod_file != ""
+	var has_mod_file: bool:
+		get:
+			return mod_file != ""
 
-    static func from_dir(dir: DirAccess, _folder: String) -> ModPath:
-        var mod_path := ModPath.new()
-        mod_path.folder = _folder
-        mod_path.mod_file = ""
-        mod_path.editor_mod_file = ""
-        dir.change_dir(_folder)
-        for file in dir.get_files():
-            if file.ends_with("_mod.pck"):
-                mod_path.mod_file = file
-        dir.change_dir("..")
-        return mod_path
+	static func from_dir(dir: DirAccess, _folder: String) -> ModPath:
+		var mod_path := ModPath.new()
+		mod_path.folder = _folder
+		mod_path.mod_file = ""
+		mod_path.editor_mod_file = ""
+		dir.change_dir(_folder)
+		for file in dir.get_files():
+			if file.ends_with("_mod.pck"):
+				mod_path.mod_file = file
+		dir.change_dir("..")
+		return mod_path
--- editor/editor_main.gd
+++ editor/editor_main.gd
@@ -6,148 +6,148 @@ const DEFAULT_EXPORT_PATH = "user://tmp/index.json"
 var editor_signals: EditorEventBus
 
 func _enter_tree():
-    # Register singleton
-    Engine.register_singleton(&"EditorSignals", EditorEventBus.new())
-    editor_signals = Engine.get_singleton(&"EditorSignals") as EditorEventBus
+	# Register singleton
+	Engine.register_singleton(&"EditorSignals", EditorEventBus.new())
+	editor_signals = Engine.get_singleton(&"EditorSignals") as EditorEventBus
 
 func _ready():
-    # File Tab
-    %NewFileButton.pressed.connect(_new_class)
-    %LoadFileButton.pressed.connect(_open_class)
-    get_tree().root.files_dropped.connect(_on_files_selected)
-    %ExportButton.pressed.connect(export_class)
-    # Handle update requests
-    editor_signals.class_index_change_requested.connect(_change_index)
-    editor_signals.class_metadata_change_requested.connect(_change_metadata)
-    editor_signals.class_script_change_requested.connect(_change_script)
-    editor_signals.class_entities_add_requested.connect(_append_entities)
-    editor_signals.class_sections_change_requested.connect(_change_sections)
-    # Load mod tabs
-    _load_mod_tabs()
-    # Update UI
-    _update_tabs()
+	# File Tab
+	%NewFileButton.pressed.connect(_new_class)
+	%LoadFileButton.pressed.connect(_open_class)
+	get_tree().root.files_dropped.connect(_on_files_selected)
+	%ExportButton.pressed.connect(export_class)
+	# Handle update requests
+	editor_signals.class_index_change_requested.connect(_change_index)
+	editor_signals.class_metadata_change_requested.connect(_change_metadata)
+	editor_signals.class_script_change_requested.connect(_change_script)
+	editor_signals.class_entities_add_requested.connect(_append_entities)
+	editor_signals.class_sections_change_requested.connect(_change_sections)
+	# Load mod tabs
+	_load_mod_tabs()
+	# Update UI
+	_update_tabs()
 
 #region Update handlers
 
 func _change_index(index: ClassIndex):
-    class_index = index
-    editor_signals.class_index_changed.emit(index)
-    _update_tabs()
+	class_index = index
+	editor_signals.class_index_changed.emit(index)
+	_update_tabs()
 
 func _change_metadata(metadata: ClassMetadata):
-    class_index.metadata = metadata
-    editor_signals.class_metadata_changed.emit(metadata)
+	class_index.metadata = metadata
+	editor_signals.class_metadata_changed.emit(metadata)
 
 func _change_script(new_script: Array[ScriptEntry]):
-    class_index.class_script = new_script
-    editor_signals.class_script_changed.emit(new_script)
+	class_index.class_script = new_script
+	editor_signals.class_script_changed.emit(new_script)
 
 func _append_entities(entities: Array[Entity]):
-    class_index.entities.append_array(entities)
-    editor_signals.class_entities_changed.emit(class_index.entities)
+	class_index.entities.append_array(entities)
+	editor_signals.class_entities_changed.emit(class_index.entities)
 
 func _change_sections(sections: Array[ClassSection]):
-    class_index.sections = sections
-    editor_signals.class_sections_changed.emit(sections)    
+	class_index.sections = sections
+	editor_signals.class_sections_changed.emit(sections)    
 
 #endregion
 
 #region Tabs
 
 func _load_mod_tabs():
-    for mod in ModLoader.mods_loaded:
-        for scene in mod.editor_scenes:
-            var instance = scene.instantiate()
-            if not is_instance_valid(instance):
-                continue
-            %EntityEditors.add_child(instance)
+	for mod in ModLoader.mods_loaded:
+		for scene in mod.editor_scenes:
+			var instance = scene.instantiate()
+			if not is_instance_valid(instance):
+				continue
+			%EntityEditors.add_child(instance)
 
 func _update_tabs():
-    var valid_file = is_instance_valid(class_index)
-    var tabs := %TabContainer as TabContainer
-    for i in tabs.get_tab_count() - 1:
-        tabs.set_tab_disabled(i + 1, not valid_file)
+	var valid_file = is_instance_valid(class_index)
+	var tabs := %TabContainer as TabContainer
+	for i in tabs.get_tab_count() - 1:
+		tabs.set_tab_disabled(i + 1, not valid_file)
 
 #endregion
 
 #region Create/Load File
 
 func _new_class():
-    _change_index(ClassIndex.new())
+	_change_index(ClassIndex.new())
 
 func _open_class():
-    if DisplayServer.has_feature(DisplayServer.FEATURE_NATIVE_DIALOG):
-        _native_dialog()
-        print("Native dialog support detected. Using native file picker.")
-        return
-    print("No custom dialog support detected. Using built-in file picker.")
-    _built_in_dialog()
+	if DisplayServer.has_feature(DisplayServer.FEATURE_NATIVE_DIALOG):
+		_native_dialog()
+		print("Native dialog support detected. Using native file picker.")
+		return
+	print("No custom dialog support detected. Using built-in file picker.")
+	_built_in_dialog()
 
 func _native_dialog():
-    DisplayServer.file_dialog_show("Open File", "", "", false, DisplayServer.FILE_DIALOG_MODE_OPEN_FILE, ["*.json"], _on_native_dialog_file_selected)
+	DisplayServer.file_dialog_show("Open File", "", "", false, DisplayServer.FILE_DIALOG_MODE_OPEN_FILE, ["*.json"], _on_native_dialog_file_selected)
 
 func _built_in_dialog():
-    var dialog := FileDialog.new()
-    add_child(dialog)
-    dialog.use_native_dialog = true
-    dialog.access = FileDialog.ACCESS_FILESYSTEM
-    dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
-    dialog.add_filter("*.json", "Poodle Index Files")
-    dialog.files_selected.connect(_on_files_selected)
-    dialog.file_selected.connect(_on_file_selected)
-    dialog.popup_centered()
+	var dialog := FileDialog.new()
+	add_child(dialog)
+	dialog.use_native_dialog = true
+	dialog.access = FileDialog.ACCESS_FILESYSTEM
+	dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+	dialog.add_filter("*.json", "Poodle Index Files")
+	dialog.files_selected.connect(_on_files_selected)
+	dialog.file_selected.connect(_on_file_selected)
+	dialog.popup_centered()
 
 func _on_native_dialog_file_selected(status: bool, selected_paths: PackedStringArray, _selected_filter_index: int) -> void:
-    if status:
-        _on_files_selected(selected_paths)
+	if status:
+		_on_files_selected(selected_paths)
 
 func _on_files_selected(files: PackedStringArray) -> void:
-    for file in files:
-        if file.ends_with(".json"):
-            _on_file_selected(file)
-            return
+	for file in files:
+		if file.ends_with(".json"):
+			_on_file_selected(file)
+			return
 
 func _on_file_selected(path: String) -> void:
-    var file := FileAccess.open(path, FileAccess.READ)
-    var content := file.get_as_text()
-    var index = JSON.parse_string(content)
-    if index == null or typeof(index) != TYPE_DICTIONARY:
-        return
-    _change_index(ClassIndex.deserialize(index))
+	var file := FileAccess.open(path, FileAccess.READ)
+	var content := file.get_as_text()
+	var index = JSON.parse_string(content)
+	if index == null or typeof(index) != TYPE_DICTIONARY:
+		return
+	_change_index(ClassIndex.deserialize(index))
 
 #endregion
 
 func export_class():
-    _compute_wrapper_index()
-    _export_class()
+	_compute_wrapper_index()
+	_export_class()
 
 func _compute_wrapper_index():
-    for section in class_index.sections:
-        for slide in section.slides:
-            _compute_wrapper_index_in_group(slide.content_root)
+	for section in class_index.sections:
+		for slide in section.slides:
+			_compute_wrapper_index_in_group(slide.content_root)
 
 func _compute_wrapper_index_in_group(group: ClassGroup):
-    for inner_group in group.groups:
-        _compute_wrapper_index_in_group(inner_group)
-    for wrapper in group.entities:
-        if not is_instance_valid(wrapper.entity):
-            continue
-        var index := class_index.entities.find(wrapper.entity)
-        if index == -1:
-            printerr("Entity not found in class index: " + wrapper.entity.get_name())
-            continue
-        wrapper.entity_id = index
+	for inner_group in group.groups:
+		_compute_wrapper_index_in_group(inner_group)
+	for wrapper in group.entities:
+		if not is_instance_valid(wrapper.entity):
+			continue
+		var index := class_index.entities.find(wrapper.entity)
+		if index == -1:
+			printerr("Entity not found in class index: " + wrapper.entity.get_name())
+			continue
+		wrapper.entity_id = index
 
 func _export_class():
-    var path: String = %ExportPath.text
-    if path.is_empty():
-        path = DEFAULT_EXPORT_PATH
-    var relative_dir := path.replace("user://", "").replace("index.json", "")
-    var dir := DirAccess.open("user://")
-    if not dir.dir_exists(relative_dir):
-        dir.make_dir_recursive(relative_dir)
-    var final_path := "user://" + relative_dir + "index.json"
-    var file := FileAccess.open(final_path, FileAccess.WRITE)
-    file.store_string(JSON.stringify(class_index.serialize(), "\t"))
-    file.close()
-    OS.shell_open(OS.get_user_data_dir() + "/" + relative_dir + "/")
+	var path: String = %ExportPath.text
+	if path.is_empty():
+		path = DEFAULT_EXPORT_PATH
+	var relative_dir := path.replace("user://", "").replace("index.json", "")
+	var dir := DirAccess.open("user://")
+	if not dir.dir_exists(relative_dir):
+		dir.make_dir_recursive(relative_dir)
+	var final_path := "user://" + relative_dir + "index.json"
+	var file := FileAccess.open(final_path, FileAccess.WRITE)
+	file.store_string(JSON.stringify(class_index.serialize(), "\t"))
+	file.close()
+	OS.shell_open(OS.get_user_data_dir() + "/" + relative_dir + "/")
